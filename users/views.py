139457491883bf811c636e02e74d2a7202ce8013
Jakhongir Rakhmonov <jrahmonov2@gmail.com>
2021-12-24 20:03:38 +0100

    add login implementation

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from django.views import View
 
-from users.forms import UserCreateForm
+from users.forms import UserCreateForm, UserLoginForm
 
 
 class RegisterView(View):
@@ -28,4 +29,17 @@ class RegisterView(View):
 
 class LoginView(View):
     def get(self, request):
-        return render(request, "users/login.html")
+        login_form = UserLoginForm()
+
+        return render(request, "users/login.html", {"login_form": login_form})
+
+    def post(self, request):
+        login_form = AuthenticationForm(data=request.POST)
+
+        if login_form.is_valid():
+            user = login_form.get_user()
+            login(request, user)
+
+            return redirect("landing_page")
+        else:
+            return render(request, "users/login.html", {"login_form": login_form})
