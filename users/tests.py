1fa505fbcbd21e542a74ff256433511801ed93d7
Jakhongir Rakhmonov <jrahmonov2@gmail.com>
2021-12-24 18:33:22 +0100

    Add registration unit tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,64 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class RegistrationTestCase(TestCase):
+    def test_user_account_is_created(self):
+        self.client.post(
+            reverse("users:register"),
+            data={
+                "username": "jakhongir",
+                "first_name": "Jakhongir",
+                "last_name": "Rakhmonov",
+                "email": "jrahmonov2@gmail.com",
+                "password": "somepassword"
+            }
+        )
+
+        user = User.objects.get(username="jakhongir")
+
+        self.assertEqual(user.first_name, "Jakhongir")
+        self.assertEqual(user.last_name, "Rakhmonov")
+        self.assertEqual(user.email, "jrahmonov2@gmail.com")
+        self.assertNotEqual(user.password, "somepassword")
+        self.assertTrue(user.check_password("somepassword"))
+
+    def test_required_fields(self):
+        response = self.client.post(
+            reverse("users:register"),
+            data={
+                "first_name": "Jakhongir",
+                "email": "jrahmonov2@gmail.com"
+            }
+        )
+
+        user_count = User.objects.count()
+
+        self.assertEqual(user_count, 0)
+        self.assertFormError(response, "form", "username", "This field is required.")
+        self.assertFormError(response, "form", "password", "This field is required.")
+
+    def test_invalid_email(self):
+        response = self.client.post(
+            reverse("users:register"),
+            data={
+                "username": "jakhongir",
+                "first_name": "Jakhongir",
+                "last_name": "Rakhmonov",
+                "email": "invalid-email",
+                "password": "somepassword"
+            }
+        )
+
+        user_count = User.objects.count()
+
+        self.assertEqual(user_count, 0)
+        self.assertFormError(response, "form", "email", "Enter a valid email address.")
+
+    def test_unique_username(self):
+        # 1. create a user
+        # 2. try to create another user with that same username
+        # 3. check that the second user was not created
+        # 4. check that the form contains the error message
+        pass
