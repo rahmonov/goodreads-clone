8f4e237b17aa0b58d67408fc304e33a9a40bcefe
Jakhongir Rakhmonov <jrahmonov2@gmail.com>
2021-12-25 13:25:22 +0100

    Books Test Case

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from books.models import Book
+
+
+class BooksTestCase(TestCase):
+    def test_no_books(self):
+        response = self.client.get(reverse("books:list"))
+
+        self.assertContains(response, "No books found.")
+
+    def test_books_list(self):
+        Book.objects.create(title="Book1", description="Description1", isbn="123121")
+        Book.objects.create(title="Book2", description="Description2", isbn="111111")
+        Book.objects.create(title="Book3", description="Description3", isbn="333333")
+
+        response = self.client.get(reverse("books:list"))
+
+        books = Book.objects.all()
+        for book in books:
+            self.assertContains(response, book.title)
+
+    def test_detail_page(self):
+        book = Book.objects.create(title="Book1", description="Description1", isbn="123121")
+
+        response = self.client.get(reverse("books:detail", kwargs={"id": book.id}))
+
+        self.assertContains(response, book.title)
+        self.assertContains(response, book.description)
