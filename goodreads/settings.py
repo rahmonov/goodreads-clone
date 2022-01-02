32c81e4b6cd2af6f6f7b1c4c6829e7e7314ec89d
Jakhongir Rakhmonov <jrahmonov2@gmail.com>
2022-01-02 22:48:15 +0100

    static files

--- goodreads/settings.py
+++ goodreads/settings.py
@@ -118,6 +118,9 @@ MEDIA_ROOT = "media-files"
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
