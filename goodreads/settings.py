231c9e6c2c0a998f0d400e5814e8b381ac9fddfd
Jakhongir Rakhmonov <jrahmonov2@gmail.com>
2022-01-03 22:28:49 +0100

    Crispy forms

--- goodreads/settings.py
+++ goodreads/settings.py
@@ -27,9 +27,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "crispy_forms",
+    "crispy_bootstrap5",
+
     'books',
     'users'
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
