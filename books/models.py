9db56242281ef5691a088840e2744effe9f88f28
Jakhongir Rakhmonov <jrahmonov2@gmail.com>
2022-01-03 20:54:17 +0100

    Book cover page

--- books/models.py
+++ books/models.py
@@ -7,6 +7,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     isbn = models.CharField(max_length=17)
+    cover_picture = models.ImageField(default="default_cover.jpg")
 
     def __str__(self):
         return self.title
