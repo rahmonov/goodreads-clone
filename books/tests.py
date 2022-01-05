88a7eb54e2ff5de48d8a8ad129312bd304615326
Jakhongir Rakhmonov <jrahmonov2@gmail.com>
2022-01-05 22:14:42 +0100

    Add pagination

--- books/tests.py
+++ books/tests.py
@@ -11,15 +11,18 @@ class BooksTestCase(TestCase):
         self.assertContains(response, "No books found.")
 
     def test_books_list(self):
-        Book.objects.create(title="Book1", description="Description1", isbn="123121")
-        Book.objects.create(title="Book2", description="Description2", isbn="111111")
-        Book.objects.create(title="Book3", description="Description3", isbn="333333")
+        book1 = Book.objects.create(title="Book1", description="Description1", isbn="123121")
+        book2 = Book.objects.create(title="Book2", description="Description2", isbn="111111")
+        book3 = Book.objects.create(title="Book3", description="Description3", isbn="333333")
 
         response = self.client.get(reverse("books:list"))
 
-        books = Book.objects.all()
-        for book in books:
+        for book in [book1, book2]:
             self.assertContains(response, book.title)
+
+        response = self.client.get(reverse("books:list") + "?page=2")
+
+        self.assertContains(response, book3.title)
 
     def test_detail_page(self):
         book = Book.objects.create(title="Book1", description="Description1", isbn="123121")
--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import ListView, DetailView
@@ -5,17 +6,23 @@ from django.views.generic import ListView, DetailView
 from books.models import Book
 
 
-class BooksView(ListView):
-    template_name = "books/list.html"
-    queryset = Book.objects.all()
-    context_object_name = "books"
+# class BooksView(ListView):
+#     template_name = "books/list.html"
+#     queryset = Book.objects.all()
+#     context_object_name = "books"
+#     paginate_by = 2
 
 
-# class BooksView(View):
-#     def get(self, request):
-#         books = Book.objects.all()
-#
-#         return render(request, "books/list.html", {"books": books})
+class BooksView(View):
+    def get(self, request):
+        books = Book.objects.all().order_by('id')
+        page_size = request.GET.get('page_size', 2)
+        paginator = Paginator(books, page_size)
+
+        page_num = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_num)
+
+        return render(request, "books/list.html", {"page_obj": page_obj})
 
 
 class BookDetailView(DetailView):
