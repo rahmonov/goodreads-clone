610887e543a5088ef040e622bb08f75d46e3faf0
Jakhongir Rakhmonov <jrahmonov2@gmail.com>
2022-01-05 23:16:05 +0100

    Search

--- books/tests.py
+++ books/tests.py
@@ -15,12 +15,13 @@ class BooksTestCase(TestCase):
         book2 = Book.objects.create(title="Book2", description="Description2", isbn="111111")
         book3 = Book.objects.create(title="Book3", description="Description3", isbn="333333")
 
-        response = self.client.get(reverse("books:list"))
+        response = self.client.get(reverse("books:list") + "?page_size=2")
 
         for book in [book1, book2]:
             self.assertContains(response, book.title)
+        self.assertNotContains(response, book3.title)
 
-        response = self.client.get(reverse("books:list") + "?page=2")
+        response = self.client.get(reverse("books:list") + "?page=2&page_size=2")
 
         self.assertContains(response, book3.title)
 
@@ -31,3 +32,23 @@ class BooksTestCase(TestCase):
 
         self.assertContains(response, book.title)
         self.assertContains(response, book.description)
+
+    def test_search_books(self):
+        book1 = Book.objects.create(title="Sport", description="Description1", isbn="123121")
+        book2 = Book.objects.create(title="Guide", description="Description2", isbn="111111")
+        book3 = Book.objects.create(title="Shoe Dog", description="Description3", isbn="333333")
+
+        response = self.client.get(reverse("books:list") + "?q=sport")
+        self.assertContains(response, book1.title)
+        self.assertNotContains(response, book2.title)
+        self.assertNotContains(response, book3.title)
+
+        response = self.client.get(reverse("books:list") + "?q=guide")
+        self.assertContains(response, book2.title)
+        self.assertNotContains(response, book1.title)
+        self.assertNotContains(response, book3.title)
+
+        response = self.client.get(reverse("books:list") + "?q=shoe")
+        self.assertContains(response, book3.title)
+        self.assertNotContains(response, book1.title)
+        self.assertNotContains(response, book2.title)
--- books/views.py
+++ books/views.py
@@ -16,13 +16,21 @@ from books.models import Book
 class BooksView(View):
     def get(self, request):
         books = Book.objects.all().order_by('id')
-        page_size = request.GET.get('page_size', 2)
+        search_query = request.GET.get('q', '')
+        if search_query:
+            books = books.filter(title__icontains=search_query)
+
+        page_size = request.GET.get('page_size', 4)
         paginator = Paginator(books, page_size)
 
         page_num = request.GET.get('page', 1)
         page_obj = paginator.get_page(page_num)
 
-        return render(request, "books/list.html", {"page_obj": page_obj})
+        return render(
+            request,
+            "books/list.html",
+            {"page_obj": page_obj, "search_query": search_query}
+        )
 
 
 class BookDetailView(DetailView):
