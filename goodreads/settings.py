b6a1d5dd342701fa202f07956afe03f204f5d41d
Jakhongir Rakhmonov <jrahmonov2@gmail.com>
2022-01-16 21:40:15 +0100

    Add static root

--- goodreads/settings.py
+++ goodreads/settings.py
@@ -137,6 +137,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = 'static-files'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
